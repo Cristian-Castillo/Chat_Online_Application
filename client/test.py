e34caee4f5f1992fafefeb96681c710f326220ee
ccarri11@ucsc.edu <ccarri11@ucsc.edu>
2021-02-17 15:06:26 -0800

    First commit - connected to server

---
+++ client/test.py
@@ -0,0 +1,45 @@
+from socket import AF_INET, socket, SOCK_STREAM
+from threading import Thread
+
+# GLOBAL CONSTANTS
+HOST = "localhost"
+PORT = 8080
+ADDR = (HOST,PORT)
+BUFSIZE = 512
+
+# GLOBAL VARIABLES
+messages = []
+
+client_socket = socket(AF_INET,SOCK_STREAM)
+client_socket.connect(ADDR)
+
+def receive_messages():
+    """
+    receive messages from server
+    :return: None
+    """
+    while True:
+        try:
+            msg = client_socket.recv(BUFSIZE).decode()
+            messages.append(msg)
+            print(msg)
+        except Exception as e:
+            print("[EXCEPTION]",e)
+            break
+
+def send_message(msg):
+    """
+    send messages to server
+    :param msg: str
+    :return: None
+    """
+    client_socket.send(bytes(msg,"utf8"))
+    if msg == "{quit}":
+        client_socket.close()
+
+
+receive_thread = Thread(target=receive_messages)
+receive_thread.start()
+
+send_message('Cris')
+send_message('Hello')
---
+++ server/server.py
@@ -0,0 +1,92 @@
+""" Server for multithreaded (asynchronous) chat application."""
+from socket import AF_INET, socket, SOCK_STREAM
+from threading import Thread
+import time
+from person import Person
+
+# GLOBAL CONSTANTS
+HOST = 'localhost'
+PORT = 8080
+ADDR = (HOST,PORT)
+MAX_CONNECTIONS = 10
+BUFSIZE = 512
+
+# GLOBAL VARIABLES
+persons = []
+SERVER = socket(AF_INET,SOCK_STREAM)
+SERVER.bind(ADDR) # set up server
+
+
+def broadcast(msg,name):
+    """
+    send new messages to all clients
+    :param msg: bytes["utf8"]
+    :param name: str
+    :return:
+    """
+    for person in persons:
+        client = person.client
+        client.send(bytes(name + ": ", "utf8") + msg) # combo of bytes and edecode it
+
+
+def client_communication(person):
+    """
+    Thread to handle all messages from client
+    :param person: socket
+    :return: None
+    """
+    client = person.client
+
+    # get persons name
+    name  = client.recv(BUFSIZE).decode("utf8")
+    msg = bytes(f"{name} has joined the chat!","utf8")
+    broadcast(msg,name) # broadcast welcome message
+
+    while True:
+        try:
+            msg = client.recv(BUFSIZE) # wait, if we receive
+            print(f"{name}: ", msg.decode("utf8"))
+
+            if msg == bytes("{quit}","utf8"):
+                broadcast(f"{name} has left the chat...", "")
+                client.send(bytes("{quit}","utf8"))
+                client.close()
+                persons.remove(person)
+                break
+            else: # broadcast their msg
+                broadcast(msg,name)
+        except Exception as e:
+            print("[EXCEPTION]",e)
+            break
+
+def wait_for_connection():
+    """
+    Wait for connection from new clients, start new thread once connected
+    : param SERVER: SOCKET
+    : return: None
+    """
+
+    run = True
+    while run:
+        try: # try to connect, else show fail msg
+            client,addr = SERVER.accept()
+            person = Person(addr,client)
+            persons.append(person) # all clients in person list
+            print(f"[CONNECTION] {addr} connected to the server at {time.time()}")
+            Thread(target=client_communication,args=(person,)).start()
+        except Exception as e:
+            print("[EXCEPTION]",e)
+            run = False
+
+    print("SERVER CRASHED")
+
+
+
+if __name__ == "__main__":
+    SERVER.listen(MAX_CONNECTIONS) # listen for connections in backlog
+    print("[STARTED] Waiting for connection...")
+    # Starts a new thread
+    ACCEPT_THREAD = Thread(target = wait_for_connection) 
+    ACCEPT_THREAD.start()
+    ACCEPT_THREAD.join()
+    SERVER.close()
